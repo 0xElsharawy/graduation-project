cac6fe9c0c09bfa07540842b24d4eba541c05ab1
ahmed-alseidy <ahmedalseidy4@gmail.com>
2026-03-07 00:04:33 +0200

    refactor: remove nextCookies()

--- apps/web/lib/auth-client.ts
+++ apps/web/lib/auth-client.ts
@@ -1,10 +1,8 @@
-import { nextCookies } from "better-auth/next-js";
 import { createAuthClient } from "better-auth/react";
 import { BACKEND_URL } from "./constants";
 
 export const authClient: ReturnType<typeof createAuthClient> = createAuthClient(
   {
     baseURL: `${BACKEND_URL}/auth`,
-    plugins: [nextCookies()],
   }
 );
